ba7867a5764cf6c8e5894baa3eecbc3e6c26ae70
TOM <157478634+todhc22skjicea@users.noreply.github.com>
2025-06-18 19:30:18 +0800

    Update vir.py

--- PBscope/vir.py
+++ PBscope/vir.py
@@ -7,8 +7,8 @@ with open('./configs/drug282.csv', mode='r') as file:
     reader = csv.reader(file)  
     c_folder = list(reader)  
 
-saved_file_path = r'/home/drug_model/result_T6/outcomes.npy'
-out_path  =r"/home/drug_model/result_T6/"
+saved_file_path = r'/home/sdx/drug_model/result_T6/outcomes.npy'
+out_path  =r"/home/sdx/drug_model/result_T6/"
 data = torch.load(saved_file_path)  
 ground_truth_labels = data['ground_truth']  
 cluster_result = data['cluster_result']  
@@ -50,27 +50,7 @@ plt.figure(figsize=(10, 8))
 
 plt.rcParams['font.family'] = 'Helvetica'  # 
 plt.rcParams['font.size'] = 28  # font
-#scatter = plt.scatter(umap_embedding[:, 0], umap_embedding[:, 1], c=ground_truth_labels, cmap='RdBu', s=50, alpha=0.8)  
-scatter = plt.scatter(umap_embedding[:, 0], umap_embedding[:, 1], c=cluster_result, cmap='viridis', s=100, alpha=0.7)  
-#for i, txt in enumerate(ground_truth_labels):  
-#    plt.annotate(txt, (umap_embedding[i, 0], umap_embedding[i, 1]),fontsize=16)  
 
-plt.colorbar(scatter, label='Cluster Label')
-plt.title('UMAP result')
-plt.xlabel('UMAP 1')  
-plt.ylabel('UMAP 2') 
-x_lim = plt.xlim()  
-y_lim = plt.ylim()
-ax = plt.gca()
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.set_xlabel('')
-ax.set_ylabel('')
-ax.set_xticks([])
-ax.set_yticks([])
-#legend1 = plt.legend(*scatter.legend_elements(), title="Clusters")  
-plt.savefig(out_path+'/test2.svg', format='svg')
-plt.close()
 for index in range(0,282):
     count1 = 0
     count2 = 0
@@ -81,21 +61,15 @@ for index in range(0,282):
     scatter = plt.scatter(umap_embedding[:, 0], umap_embedding[:, 1],label='Total',c='whitesmoke', s=100, alpha=0.7)
     
   
-      for number in range(0,len(cluster_result)):
+    for number in range(0,len(cluster_result)):
       
-        if (c_folder[0][ground_truth_labels[number]] == "DMSO" or c_folder[0][ground_truth_labels[number]] == "NA"):
-  
-      if (c_folder[0][ground_truth_labels[number]] == "DMSO"):
+      if (c_folder[0][ground_truth_labels[number]] == "DMSO" or c_folder[0][ground_truth_labels[number]] == "NA"):
         if count2 == 0:
           scatter = plt.scatter(umap_embedding[number, 0], umap_embedding[number, 1],label='negative control',c='goldenrod',s=100, alpha=0.7)
           count2 = count2+1
         else:
    
           scatter = plt.scatter(umap_embedding[number, 0], umap_embedding[number, 1],c='goldenrod',s=100, alpha=0.7)
-    #for number in range(0,len(cluster_result)):
-    #  if ground_truth_labels[number] == 282:
-    #    scatter = plt.scatter(umap_embedding[number, 0], umap_embedding[number, 1],c='goldenrod',s=30, alpha=0.5) 
-  
     drug_embedding = []
     for number in range(0,len(cluster_result)):
       if ground_truth_labels[number] == index:
